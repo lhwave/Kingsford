76e6ddcba13bddaf2f3dd4080220c5544482fb14
lhwave <lhwave@gmail.com>
2024-08-20 17:27:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import streamlit as st
 import plotly.express as px
 
 df = pd.read_excel(io='https://raw.githubusercontent.com/lhwave/kingsford/main/Data.xlsx', sheet_name='PL', usecols='A:F')
-df_gp = df.groupby(by=['Month','Year']).sum()[['Amount']]
 
 Months = df['Month'].unique().tolist()
 Month_selection = st.slider("Month selection:", min_value=min(Months), max_value=max(Months), value=(min(Months),max(Months)))
 
-fig = px.histogram(df, x='Month', y='Amount',
+mask = (df['Month'].between(*Month_selection))
+
+df_gp = df[mask].groupby(by=['Month','Year']).sum()[['Amount']]
+
+fig = px.histogram(df_gp, x='Month', y='Amount',
              color='Year', barmode='group')
 
 st.plotly_chart(fig)
