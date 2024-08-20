e0f7a8da30110028ee7da59af7b734ec0114f1a0
lhwave <lhwave@gmail.com>
2024-08-20 16:05:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-df = pd.read_excel(io='https://raw.githubusercontent.com/lhwave/kingsford/main/Data.xlsx', sheet_name='PL')
+df = pd.read_excel(io='https://raw.githubusercontent.com/lhwave/kingsford/main/Data.xlsx', sheet_name='PL', usecols='A:F')
 df_gp = df.groupby(by=['Month'],['Year']).sum()[['Amount']]
 
 fig = px.histogram(df_gp, x="Month", y="Amount",
