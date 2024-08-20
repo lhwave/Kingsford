235d5f954a2d5d0b995e3ff2f7d8c5b4f0238477
lhwave <lhwave@gmail.com>
2024-08-20 16:58:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 df = pd.read_excel(io='https://raw.githubusercontent.com/lhwave/kingsford/main/Data.xlsx', sheet_name='PL', usecols='A:F')
 df_gp = df.groupby(by=['Month','Year']).sum()[['Amount']]
 
-fig = px.histogram(df, x="Month", y="Amount",
-             color="Year", barmode="group")
+fig = px.histogram(df, x='Month', y='Amount',
+             color='Year', barmode='group')
 
 st.plotly_chart(fig)
