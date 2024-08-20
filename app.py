b7a788ff1e034b47a33f7f848d9321678e47b78e
lhwave <lhwave@gmail.com>
2024-08-20 11:31:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_excel(r'C:\Users\User\Documents\Excel_Webapp\Data.xlsx', sheet_name='PL')
+df = pd.read_excel(io='https://raw.githubusercontent.com/lhwave/kingsford/main/Data.xlsx', sheet_name='PL')
 st.dataframe(df)
