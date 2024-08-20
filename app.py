de0bb8858dd1e367a0c047615e5fa873fd0f4d54
lhwave <lhwave@gmail.com>
2024-08-20 15:53:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ import streamlit as st
 import plotly.express as px
 
 df = pd.read_excel(io='https://raw.githubusercontent.com/lhwave/kingsford/main/Data.xlsx', sheet_name='PL')
-df_gp = df.groupby(by=['Account Type']).sum()[['Amount']]
-st.dataframe(df_gp)
+df_gp = df.groupby(by=['Month'],['Year']).sum()[['Amount']]
+
+fig = px.histogram(df_gp, x="Month", y="Amount",
+             color='Year', barmode='group',
+             height=400)
+
+st.plotly_chart(fig)
