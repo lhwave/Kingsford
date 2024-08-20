7652ac67ae9f051249aea6d3435f17a3ca3c9d67
lhwave <lhwave@gmail.com>
2024-08-20 12:34:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 import plotly.express as px
 
 df = pd.read_excel(io='https://raw.githubusercontent.com/lhwave/kingsford/main/Data.xlsx', sheet_name='PL')
-df_gp = df[mask].groupby(by=['Account Type']).sum()[['Amonut']]
+df_gp = df[mask].groupby(by=['Acount Type']).sum()[['Amonut']]
 st.dataframe(df_gp)
