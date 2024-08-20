2e319ddfaf27fe34a943be04c65cf21104b3b492
lhwave <lhwave@gmail.com>
2024-08-20 17:15:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 df = pd.read_excel(io='https://raw.githubusercontent.com/lhwave/kingsford/main/Data.xlsx', sheet_name='PL', usecols='A:F')
 df_gp = df.groupby(by=['Month','Year']).sum()[['Amount']]
 
-Months = df[‘Month’].unique().tolist()
+Months = df['Month'].unique().tolist()
 Month_selection = st.slider("Month selection:", min_value=min(Months), max_value=max(Months), value=(min(Months),max(Months)))
 
 fig = px.histogram(df, x='Month', y='Amount',
