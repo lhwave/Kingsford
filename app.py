53a18c3e24e69591abb8dce0ccbec6ca93420697
lhwave <lhwave@gmail.com>
2024-08-24 10:13:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.subheader('Income')
 col1, col2 = st.columns(2)
 
 with col1:
-Year_selection = st.radio(key="Year selection",options=[Years])
+st.radio(key="Year selection",options=[Years])
 
 mask= (df_Income['Month'].between(*Month_selection)) & (df_Income['Year']==(Year_selection)
 df_Income_gp= df_Income[mask].groupby(by=['Account Name']).sum()[['Amount']]
