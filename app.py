de03193caa4b21005c0bbaa720e04e3f1ca37678
lhwave <lhwave@gmail.com>
2024-08-27 12:53:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,10 +47,10 @@ col1, col2 = st.columns(2)
 with col1:
   Year_selection = st.radio("Year: ", Years,)
 
-mask= (df_Expense['Month'].between(*Month_selection)) & (df_Income['Year']==(Year_selection))
-df_Income_gp= df_Income[mask].groupby(by=['Account Name']).sum()[['Amount']]
-df_Income_gp= df_Income_gp.reset_index()
-fig = px.pie(df_Income_gp, values='Amount', names = 'Account Name')
+mask= (df_Expense['Month'].between(*Month_selection)) & (df_Expense['Year']==(Year_selection))
+df_Expense_gp= df_Expense[mask].groupby(by=['Account Name']).sum()[['Amount']]
+df_Expense_gp= df_Expense_gp.reset_index()
+fig = px.pie(df_Expense_gp, values='Amount', names = 'Account Name')
 
 with col2:
   st.plotly_chart(fig)
